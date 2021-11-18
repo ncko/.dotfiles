cfb72c54f29f2bcc7fcfdbfee4e5169695449c9e
ncko <hello@ncko.me>
2021-11-18 11:03:57 -0800

    feat: automatic node version switching

--- omz-custom/environment.zsh
+++ omz-custom/environment.zsh
@@ -4,11 +4,27 @@ export PATH=/opt/homebrew/bin:$PATH
 # Created by `pipx` on 2021-10-11 05:20:52
 export PATH="$PATH:/Users/$USER/.local/bin"
 
-# load nvm
+
+###
+# NVM
+###
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# https://medium.com/@kinduff/automatic-version-switch-for-nvm-ff9e00ae67f3#:~:text=The%20automatic%20switch%20will%20switch,shell%20to%20make%20this%20work.
+autoload -U add-zsh-hook
+load-nvmrc() {
+  if [[ -f .nvmrc && -r .nvmrc ]]; then
+    nvm use
+  elif [[ $(nvm version) != $(nvm version default)  ]]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
 
 
 
