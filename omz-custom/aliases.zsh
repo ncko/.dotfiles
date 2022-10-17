49b6581bb9777607903fcf4edb9983455e1f4517
ncko <hello@ncko.me>
2022-10-17 10:16:45 -0700

    vim aliases for dotfiles

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -2,7 +2,9 @@
 alias vim="nvim"
 alias sed="gsed"
 alias awk="gawk"
-
+alias vimconfig="vim ~/.dotfiles/nvim/.config/nvim/lua/ncko"
+alias vimbin="vim ~/.dotfiles/bin/.local/bin"
+alias vimdot="vim ~/.dotfiles"
 
 
 
