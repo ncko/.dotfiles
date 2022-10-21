703d4624263f7062b45696c3496e2c23b77bb088
ncko <hello@ncko.me>
2022-10-20 19:44:41 -0700

    terraform/hcl with lspconfig

--- nvim/.config/nvim/lua/ncko/init.lua
+++ nvim/.config/nvim/lua/ncko/init.lua
@@ -8,3 +8,79 @@ vim.cmd "colorscheme gruvbox"
 require("ncko.whichkey").setup()
 require("ncko.treesitter").setup()
 
+require("mason").setup()
+require("mason-lspconfig").setup({ ensure_installed = { "sumneko_lua" } })
+-- lsp config setup goes here
+
+-- add additional capabilities supported by nvim-cmp
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require("lspconfig")
+-- enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { "pyright", "tsserver", "sumneko_lua" }
+for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+        -- on_attach = my_custom_on_attach
+        capabilities = capabilities
+    }
+end
+
+-- luasnip setup
+local luasnip = require("luasnip")
+
+-- nvim-cmp setup
+local cmp = require("cmp")
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
+
+-- https://mukeshsharma.dev/2022/02/08/neovim-workflow-for-terraform.html
+-- recognize the hcl and terraform filetype
+vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
+
+-- automatically format *.tf and *.tfvars files with terraform fmt on save and align settings.
+vim.cmd([[let g:terraform_fmt_on_save=1]])
+vim.cmd([[let g:terraform_align=1]])
+
+-- call the terraformls and tflint PID to attach as client to the Neovim LSP
+require'lspconfig'.terraformls.setup{}
+require'lspconfig'.tflint.setup{}
