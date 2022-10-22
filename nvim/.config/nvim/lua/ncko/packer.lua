be2d6eb29edd3f8518659341931041792b8c71db
ncko <hello@ncko.me>
2022-10-22 10:13:08 -0700

    Never gonna give, never gonna give

--- nvim/.config/nvim/lua/ncko/packer.lua
+++ nvim/.config/nvim/lua/ncko/packer.lua
@@ -9,7 +9,8 @@ return require("packer").startup(function()
     use("hrsh7th/nvim-cmp") -- autocompletion plugin
     use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
     use("saadparwaiz1/cmp_luasnip") -- snippets source for nvim-cmp
-    use("L3MON4D3/LuaSnip") -- Snippets plugin
+    use({ "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*" }) -- Snippets plugin
+    use("rafamadriz/friendly-snippets")
 
     use("folke/which-key.nvim")
     -- use("sbdchd/neoformat") -- still trying to get this to work
---
+++ nvim/.config/nvim/lua/ncko/snippets.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+-- https://github.com/rafamadriz/friendly-snippets/tree/main/snippets
+local snippets = {
+    "html",
+    "gitcommit",
+    "markdown",
+    "php",
+    "shell",
+    -- "python",
+    -- "css",
+    -- "scss",
+    -- "html",
+    -- "lua",
+    -- "sql",
+    -- "javascript",
+}
+
+function M.setup()
+    require("luasnip.loaders.from_vscode").load({ include = snippets })
+    require("luasnip.loaders.from_vscode").load({ paths = { "./ncko-snippets" } })
+end
+
+return M
