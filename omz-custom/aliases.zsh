de43aa65db800abbd0bfc09d52978368dabade87
ncko <nick.olsen@crossfit.com>
2022-11-01 16:37:08 -0700

    Add tldrf alias

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -8,10 +8,13 @@ alias vimcfbin="vim ~/projects/ncko/crossfit/.local/bin"
 alias vimdot="vim ~/.dotfiles"
 alias commitrand='git commit -m "$(curl -sk http://whatthecommit.com/index.txt)"'
 
+
 # save last command
 alias slc="fc -ln -1 | sed 's/^\*//' >> $HOME/.cache/ncko/saved_cmds"
 # grep saved commands
 alias slg="< $HOME/.cache/ncko/saved_cmds grep"
+
+alias tldrf='tldr --list | fzf --preview "tldr {1} --color=always" --preview-window=right,70% --layout reverse | xargs tldr'
 
 # key bindings
 bindkey -s ^f "tms\n"
