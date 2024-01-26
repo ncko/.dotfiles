aebd9741ee8cbf534fd0053b04d08c468f8c509c
ncko <hello@ncko.me>
2024-01-25 18:33:48 -0800

    comment out aliases for gcc

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -19,7 +19,7 @@ bindkey -s ^f "tms\n"
 bindkey '^ ' autosuggest-accept
 
 # gcc
-alias gcc="gcc-13"
-alias cc="gcc-13"
-alias g++="g++-13"
-alias c++="c++-13"
+#alias gcc="gcc-13"
+#alias cc="gcc-13"
+#alias g++="g++-13"
+#alias c++="c++-13"
